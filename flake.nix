a28925aeafc7f7bafe293732a62c008fa1c68a26
Alex Kwiatkowski <alex+git@fremantle.io>
2026-03-09 11:48:49 -0700

    build: remove zvec-sys and C++ toolchain dependencies
    This fully drops the stale zvec integration and prepares the repository to produce static Rust binaries by removing heavy C++ toolchain dependencies like cmake and GCC from the devShell.

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs-cmake.url = "github:NixOS/nixpkgs/nixos-25.05";
     keel = {
       url = "github:rupurt/keel?rev=2b14d578c1771b247275ad5ba295e2cf4a0f1fec";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -19,14 +18,12 @@
     nixpkgs,
     rust-overlay,
     flake-utils,
-    nixpkgs-cmake,
     keel,
   }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
-        compatPkgs = import nixpkgs-cmake { inherit system; };
         rust = pkgs.rust-bin.stable.latest.default.override {
           extensions = [ "rust-src" "rust-analyzer" ];
         };
@@ -35,31 +32,15 @@
 
         sharedInputs = [
           rust
-          compatPkgs.cmake
-          pkgs.ninja
-          pkgs.gnumake
-          compatPkgs.stdenv.cc
           pkgs.pkg-config
           pkgs.just
           pkgs.cargo-nextest
-          pkgs.zlib
           pkgs.openssl
-          pkgs.lz4
-          pkgs.zstd
-          pkgs.snappy
-          pkgs.bzip2
-          pkgs.gflags
-          pkgs.glog
-          pkgs.protobuf
-          pkgs.re2
-          pkgs.python3
-          pkgs.perl
           keelPkg
         ];
 
         linuxInputs = pkgs.lib.optionals isLinux [
           pkgs.mold
-          compatPkgs.stdenv.cc.cc.lib
         ];
       in {
         packages = {
@@ -72,9 +53,6 @@
 
           shellHook = ''
             export CARGO_TARGET_DIR="$HOME/.cache/cargo-target/sift"
-            export CC="${compatPkgs.stdenv.cc}/bin/cc"
-            export CXX="${compatPkgs.stdenv.cc}/bin/c++"
-            export LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib"
           '' + pkgs.lib.optionalString isLinux ''
             export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_RUSTFLAGS="-C link-arg=-fuse-ld=mold"
             export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_RUSTFLAGS="-C link-arg=-fuse-ld=mold"
--- third_party/zvec-sys/wrapper.h
+++
@@ -1,1 +0,0 @@
-#include "zvec_c.h"
